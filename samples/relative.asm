05e209c1cd6ddd435662b233a2fdd69662dbe42d
Starscouts <me@floo.fi>
2024-11-30 12:14:16 +0100

    Add more sample assembly

---
+++ samples/relative.asm
@@ -0,0 +1,11 @@
+        jsr foo
+        int
+
+foo:
+        ldx #12                 ; Set X to 12
+        ldy #34                 ; Set Y to 34
+        add X, Y                ; 12 + 34
+        bne #2                  ; Immediately return
+        lda $1000               ; Load from 0x1000
+        mov A, R6               ; Move to R6
+        ret
