6387c23cf5741d3bc38deb2b8635aa8a529ab858
Starscouts <me@floo.fi>
2024-12-06 11:00:47 +0100

    Refactor assembler a bit

---
+++ assembler/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs;
+use std::path::PathBuf;
+use std::process::exit;
+use vasm::scanner::Scanner;
+use clap::Parser;
+use vasm::error::Result;
+
+/// A code assembler for Vixen processors
+#[derive(Parser, Debug)]
+#[command(about)]
+struct Args {
+    pub source: PathBuf,
+}
+
+fn run_assembler(args: Args) -> Result<()> {
+    let source = fs::read_to_string(args.source)?;
+    let tokens = Scanner::new(&source).scan();
+
+    println!("{tokens:#?}");
+    Ok(())
+} 
+
+fn main() {
+    let args = Args::parse();
+    if let Err(e) = run_assembler(args) {
+        eprintln!("error: {e}");
+        exit(1);
+    }
+}
