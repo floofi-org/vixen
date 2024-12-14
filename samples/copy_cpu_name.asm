29834248b0d2b3d234abd4b43bdb04552394a9cd
Starscouts <me@floo.fi>
2024-12-14 18:55:25 +0100

    Fix code sample without include

--- samples/copy_cpu_name.asm
+++ samples/copy_cpu_name.asm
@@ -1,8 +1,10 @@
 main:
-    and $04500200, $FF ; CPU name length, AND to take only one byte
+    and $00000000, #$FF ; CPU name length, AND to take only one byte
     mov r1, r0
-    mov r2, $00000001 ; Start of the CPU name
-    mov r3, $04500200 ; Copy to the start of the RAM
+    ldr #$00000001 ; Start of the CPU name
+    mov r2, r0
+    ldr #$04500200 ; Copy to the start of the RAM
+    mov r3, r0
     jsr strcopy
 
 ; Arguments
@@ -14,12 +16,12 @@ strcopy:
     ; If so return
     mov r0, r1
     cmp #0
-    bne +2
+    bne +30
     ret
 
     ; Copy character to destination
-    ldr r2
-    str r3
+    ldr [r2]
+    str [r3]
 
     ; Increment pointers and decrement remaining string length
     inc r2
