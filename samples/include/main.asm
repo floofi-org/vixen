1256b1ff8b7efa0b59304ae976471b41b592586c
Ryze <50497128+ryze312@users.noreply.github.com>
2024-12-22 15:52:41 +0300

    arch: Fix control flow instructions taking address wrong

--- samples/include/main.asm
+++ samples/include/main.asm
@@ -2,7 +2,7 @@ main:
     and r1, $00000000, #$FF ; CPU name length, AND to take only one byte
     mov r2, #$00000001 ; Start of the CPU name
     mov r3, #$04500200 ; Copy to the start of the RAM
-    jsr strcopy
+    jsr #strcopy
     int
 
 .include "strcopy.asm"
--- samples/include/strcopy.asm
+++ samples/include/strcopy.asm
@@ -6,16 +6,17 @@ strcopy:
     ; Check whether the string length is zero
     ; If so return
     cmp r1, #0
-    bne +30
+    bne #copy
     ret
 
-    ; Copy character to destination, taking only one byte
-    and [r3], [r2], #$FF
+    copy:
+        ; Copy character to destination
+        and [r3], [r2], #$FF
 
-    ; Increment pointers and decrement remaining string length
-    inc r2
-    inc r3
-    dec r1
+        ; Increment pointers and decrement remaining string length
+        inc r2
+        inc r3
+        dec r1
 
-    ; Move next
-    jmp strcopy
+        ; Move next
+        jmp #strcopy
